60f8586a45babc0222ec88d37c9fb2891160b596
LiamCHayes <liamcharleshayes@gmail.com>
2025-10-27 17:38:49 -0400

    lh

--- labs/lab4/mytraffic.c
+++ labs/lab4/mytraffic.c
@@ -368,12 +368,13 @@ static ssize_t mytraffic_read(struct file *filp, char *buf, size_t count, loff_t
     size_t len;
     buf_max += 96;
 
-    len = scnprintf(buffer, buf_max, "Current operational mode: %s\nCurrent cycle rate: %d Hz\nRed = %s\nYellow = %s\nGreen = %s\nPedestrians are %s\n", current_op_mode, cycle_mod_HZ, red_status, yellow_status, green_status, ped_status);
+    len = scnprintf(buffer, buf_max, "Current operational mode: %s\nCurrent cycle rate: %d Hz\nRed = %s\nYellow = %s\nGreen = %s\nPedestrians are %s", current_op_mode, cycle_mod_HZ, red_status, yellow_status, green_status, ped_status);
     buffer[len] = '\0';
 
     printk(KERN_ALERT "%s\n", buffer);
 
-    *f_pos = len + 1;
+    printk(KERN_ALERT "test\n");
+    *f_pos = len;
 
     return len;
 }
