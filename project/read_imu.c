dce5953b0104294895c7978adb385df0fc0bad75
LiamCHayes <liamcharleshayes@gmail.com>
2025-11-21 01:43:18 -0500

    test

--- project/read_imu.c
+++ project/read_imu.c
@@ -137,7 +137,7 @@ int main() {
         }
 
         int first_bit = status_data[0] >> (CHAR_BIT - 1) & 1;
-        printf("Status data: %s\n", status_data);
+        printf("Status data: %d\n", status_data);
         printf("Data ready: %d\n", first_bit);
         if (!first_bit) {
             continue;
