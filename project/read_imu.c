c87a9520fd76fa0a6d9bb39f8dace96229101742
LiamCHayes <liamcharleshayes@gmail.com>
2025-11-20 22:05:29 -0500

    test

--- project/read_imu.c
+++ project/read_imu.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -29,26 +30,26 @@ int main() {
 	    exit(1);
 	}
 
-    for (int i=0; i < 10000; i++) {
+    for (int i=0; i < 1000; i++) {
         char reg = 0x3B; // Accelerometer X-axis high byte register
         if (write(file, &reg, 1) != 1) {
             perror("Failed to write to the i2c bus");
             exit(1);
         }
 
-        char data[6] = {0}; // For X, Y, Z accelerometer data (high
+        unsigned char data[6] = {0}; // For X, Y, Z accelerometer data (high
         if (read(file, data, 6) != 6) {
             perror("Failed to read from the i2c bus");
             exit(1);
         }
 
         // Process data (e.g., combine high and low bytes, convert to
-        int accelX = (data[0] << 8) | data[1];
-        int accelY = (data[2] << 8) | data[3];
-        int accelZ = (data[4] << 8) | data[5];
+        int16_t accelX = (data[0] << 8) | data[1];
+        int16_t accelY = (data[2] << 8) | data[3];
+        int16_t accelZ = (data[4] << 8) | data[5];
 
         printf("Acceleration X: %d, Y: %d, Z: %d\n", accelX, accelY, accelZ);
-        usleep(1000);
+        usleep(100000);
     }
 
 	close(file);
