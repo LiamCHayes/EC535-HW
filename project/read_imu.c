09eb8bccc346c145a79430bd0617d375af0e7257
LiamCHayes <liamcharleshayes@gmail.com>
2025-11-20 23:07:15 -0500

    test

--- project/read_imu.c
+++ project/read_imu.c
@@ -23,23 +23,36 @@ int main() {
 	}
 
     // Make sure imu can send non0 value
-    char who_am_i_reg = 0x0F;
-    char who_am_i_data = {0};
+    char reg_address = 0x0F;
+    char data_buffer = {0};
 
-    // Write the register address we want to read from
-    if (write(file, &who_am_i_reg, 1) != 1) {
-        perror("Failed to write who am I register address");
+    // Structure for I2C message transaction
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[2];
+
+    // Message 1: Write the register address we want to read from
+    messages[0].addr  = addr; // Slave address 0x68
+    messages[0].flags = 0;    // Flags = 0 for write
+    messages[0].len   = 1;    // Length of message (1 byte: the register address)
+    messages[0].buf   = &reg_address; // Pointer to the register address variable
+
+    // Message 2: Read 1 byte of data back
+    messages[1].addr  = addr;
+    messages[1].flags = I2C_M_RD; // Flags = I2C_M_RD for read
+    messages[1].len   = 1;        // Length of message (1 byte: the response)
+    messages[1].buf   = &data_buffer; // Pointer to where we store the response
+
+    // Set up the packets structure
+    packets.msgs = messages;
+    packets.nmsgs = 2; // We have two messages in this transaction
+
+    // Perform the combined I2C transaction
+    if (ioctl(file, I2C_RDWR, &packets) < 0) {
+        perror("Failed to execute I2C_RDWR command");
         exit(1);
     }
 
-    // Read one byte of data
-    if (read(file, &who_am_i_data, 1) != 1) {
-        perror("Failed to read who am I data");
-        exit(1);
-    }
-
-    printf("Who Am I register value: 0x%X\n", who_am_i_data);
-
+    printf("Who Am I register value: 0x%X\n", data_buffer);
 
 	char config[2] = {0};
 	config[0] = 0x6B;
@@ -49,7 +62,7 @@ int main() {
 	    exit(1);
 	}
 
-    for (int i=0; i < 10000; i++) {
+    for (int i=0; i < 10; i++) {
         char reg = 0x3B; // Accelerometer X-axis high byte register
         if (write(file, &reg, 1) != 1) {
             perror("Failed to write to the i2c bus");
