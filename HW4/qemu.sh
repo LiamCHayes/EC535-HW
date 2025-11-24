f105908a057ccddb2ff84cff5a249a88f8413703
Hayes <liamh@vlsi33.bu.edu>
2025-11-24 13:30:50 -0500

    ok

---
+++ HW4/qemu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "[INFO] Building modules..."
+cd liamh_lab3/ul/
+make
+cd ../km
+make
+cd ../../
+
+cp -r liamh_lab3/ rootfs/root
+cp attack.sh rootfs/root
+./rebuild-rootfs.sh
+
+qemu_system-arm -M virt -m 512 -kernel stock-zImage -nographic \
+	  -drive file=rootfs.img,if=none,format=raw,id=rfs \
+	    -device virtio-blk-device,drive=rfs \
+	      -append "earlyprintk=serial root=/dev/vda"
