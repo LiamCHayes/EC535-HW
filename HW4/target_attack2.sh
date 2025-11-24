54af29ddb1a16ef53005c2d2926e57e3cb7fc64b
LiamCHayes <liamcharleshayes@gmail.com>
2025-11-24 17:01:47 -0500

    lkh

--- HW4/target_attack2.sh
+++ HW4/target_attack2.sh
@@ -7,7 +7,7 @@ insmod $1/mytimer.ko
 
 $1/ktimer -s 2 "Timer 1" &
 $1/ktimer -s 2 "Timer 2" &
-$1/ktimer -s 2 "Timer 1" &
-$1/ktimer -s 2 "Timer 1" &
-$1/ktimer -s 2 "Timer 2" &
-$1/ktimer -s 2 "Timer 2" &
+$1/ktimer -s 2 "Timer 3" &
+$1/ktimer -s 2 "Timer 4" &
+$1/ktimer -s 2 "Timer 5" &
+$1/ktimer -s 2 "Timer 6" &
