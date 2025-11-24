9e5a53cdae989cb2eaa07154099af357b841efe5
LiamCHayes <liamcharleshayes@gmail.com>
2025-11-24 09:42:19 -0500

    paths

--- HW4/attack.sh
+++ HW4/attack.sh
@@ -3,7 +3,7 @@
 # Make the device node
 mknod /dev/mytimer c 61 0
 # Load the kernel module
-insmod km/mytimer.ko
+insmod liamh_lab3/km/mytimer.ko
 
 # Try to access data from a null pointer
 ul/ktimer -s 2 "Timer 1" &
